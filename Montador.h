04807572fe8dcf3f18011653c3f9b8173409ae5c
Ian Nery Bandeira <ian.muxis@gmail.com>
2020-09-25 18:39:32 -0300

    dia 1 - diretiva de macros quase pronta

--- Montador.h
+++ Montador.h
@@ -19,7 +19,12 @@ private:
     void preprocess();
     void mount();
     void macro_handler();
+    void macro_identifier();
+    void macro_expander();
     string asm_path, preprocessed_path, mounted_path, line;
+
+    vector<string> macro_command_list1, macro_command_list2;
+    string macro_label1, macro_label2, macro_command;
 };
 
 
