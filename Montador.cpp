68327deb4d881b6e9a739606957eb493cf325036
Ian Nery Bandeira <ian.muxis@gmail.com>
2020-09-26 11:13:59 -0300

    Arrumando a merda que o autoformatador fez nas minhas chaves

--- Montador.cpp
+++ Montador.cpp
@@ -9,48 +9,38 @@ using namespace std;
 
 #define PC_START 1
 
-Montador::Montador(string asm_path_to_file)
-{
+Montador::Montador(string asm_path_to_file){
     this->asm_path = asm_path_to_file;
     this->preprocessed_path = this->asm_path.substr(0, this->asm_path.find("asm")) + "pre";
     this->mounted_path = this->asm_path.substr(0, this->asm_path.find("asm")) + "obj";
 }
 
-void Montador::inicializar_processo(string command)
-{
-    if (command == "-o")
-    {
+void Montador::inicializar_processo(string command){
+    if (command == "-o"){
         this->mount();
     }
-    else if (command == "-p")
-    {
+    else if (command == "-p"){
         this->preprocess();
     }
-    else
-    {
+    else{
         cerr << "Erro no comando!" << endl;
     }
 }
 
-void Montador::preprocess()
-{
+void Montador::preprocess(){
     ifstream asm_file;
     ofstream preprocessed_file;
 
     asm_file.open(this->asm_path);
     preprocessed_file.open(this->preprocessed_path);
-    if (!asm_file.is_open())
-    {
+    if (!asm_file.is_open()){
         cerr << "Erro na abertura do arquivo .asm";
     }
-    else if (!preprocessed_file.is_open())
-    {
+    else if (!preprocessed_file.is_open()){
         cerr << "Erro na abertura do arquivo .pre";
     }
-    else
-    {
-        while (asm_file.good())
-        {
+    else{
+        while (asm_file.good()){
             //PEGA LINHA
             getline(asm_file, this->line);
             //RETIRA COMENTÁRIOS
@@ -61,8 +51,7 @@ void Montador::preprocess()
                 this->line.end(),
                 this->line.begin(),
                 ::toupper);
-            if (!this->line.empty())
-            {
+            if (!this->line.empty()){
                 preprocessed_file << this->line + "\n";
             }
         }
@@ -72,48 +61,39 @@ void Montador::preprocess()
     }
 }
 
-void Montador::macro_identifier()
-{
+void Montador::macro_identifier(){
     int macro_count = 0;
     ifstream preprocessed_file;
 
     preprocessed_file.open(this->preprocessed_path);
-    if (preprocessed_file.is_open())
-    {
-        while (preprocessed_file.good())
-        {
+    if (preprocessed_file.is_open()){
+        while (preprocessed_file.good()){
             getline(preprocessed_file, this->line);
             // ROTINA PARA CASO ACHE A(S) DIRETIVA(S) DE MACRO
             // ta feio e mal optimizado, mas funciona!!
             // o ian do futuro vai arrumar, eu tenho certeza
-            if (this->line.find("MACRO") != std::string::npos)
-            {
+            if (this->line.find("MACRO") != std::string::npos){
                 macro_count++;
-                if (macro_count == 1)
-                {
+                if (macro_count == 1){
                     this->macro_label1 = this->line.substr(0, this->line.find(":"));
 
-                    while (this->line.find("ENDMACRO") == std::string::npos)
-                    {
+                    while (this->line.find("ENDMACRO") == std::string::npos){
                         getline(preprocessed_file, this->line);
                         this->macro_command = this->line;
                         this->macro_command_list1.push_back(this->macro_command);
                     }
                     this->macro_command_list1.pop_back();
                 }
-                else if (macro_count == 2)
-                {
+                else if (macro_count == 2){
                     this->macro_label2 = this->line.substr(0, this->line.find(":"));
-                    while (this->line.find("ENDMACRO") == std::string::npos)
-                    {
+                    while (this->line.find("ENDMACRO") == std::string::npos){
                         getline(preprocessed_file, this->line);
                         this->macro_command = this->line;
                         this->macro_command_list2.push_back(this->macro_command);
                     }
                     this->macro_command_list2.pop_back();
                 }
-                else
-                {
+                else{
                     cerr << "Existem mais do que dois macros no programa." << endl;
                 }
             }
@@ -122,35 +102,30 @@ void Montador::macro_identifier()
     preprocessed_file.close();
 }
 
-void Montador::macro_expander()
-{
+void Montador::macro_expander(){
     int macro_position_begin, macro_position_end, macro_begin_flag = 0;
     ifstream preprocessed_file_in;
     ofstream preprocessed_file_out;
     string command_line;
     vector<string> command_list;
     preprocessed_file_in.open(this->preprocessed_path);
-    if (preprocessed_file_in.is_open())
-    {
-        while (preprocessed_file_in.good())
-        {
+    if (preprocessed_file_in.is_open()){
+        while (preprocessed_file_in.good()){
             getline(preprocessed_file_in, this->line);
             if(!this->line.empty()){
                 command_list.push_back(this->line);
             }
         }
         preprocessed_file_in.close();
-        for (int i = 0; i < command_list.size(); i++)
-        {
+        for (int i = 0; i < command_list.size(); i++){
             command_line = command_list.at(i);
             // RETIRA AS DIRETIVAS DE MACRO DO VETOR PARA PASSAR PARA O ARQUIVO
-            if (command_line.find("MACRO") != std::string::npos && !macro_begin_flag)
-            {
+            if (command_line.find("MACRO") != std::string::npos 
+            && !macro_begin_flag){
                 macro_position_begin = i;
                 macro_begin_flag = 1;
             }
-            else if (command_line.find("ENDMACRO") != std::string::npos)
-            {
+            else if (command_line.find("ENDMACRO") != std::string::npos){
                 macro_position_end = i;
                 command_list.erase(
                     command_list.begin() + macro_position_begin,
@@ -158,12 +133,11 @@ void Montador::macro_expander()
                 macro_begin_flag = 0;
             }
         }
-        for (int i = 0; i < command_list.size(); i++)
-        {
+        for (int i = 0; i < command_list.size(); i++){
             command_line = command_list.at(i);
             // EXPANDE AS MACROS DA LABEL 1
-            if (command_line.find(this->macro_label1) != std::string::npos && command_line.find(":") == std::string::npos)
-            {
+            if (command_line.find(this->macro_label1) != std::string::npos 
+            && command_line.find(":") == std::string::npos){
                 command_list.erase(command_list.begin() + i);
                 command_list.insert(
                     command_list.begin() + i,
@@ -182,13 +156,11 @@ void Montador::macro_expander()
     }
 }
 
-void Montador::macro_handler()
-{
+void Montador::macro_handler(){
     macro_identifier();
     macro_expander();
 }
 
-void Montador::mount()
-{
+void Montador::mount(){
     cout << "SoonTM" << endl;
 }
